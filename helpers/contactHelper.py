3e34d4284c5acbb30227e2556b8518ac65a1b2b1
Garret Munoz <gmunoz@uic.edu>
2017-08-29 14:10:57 -0500

    added contactTypeCounter

--- helpers/contactHelper.py
+++ helpers/contactHelper.py
@@ -31,4 +31,18 @@ def pivotRecentContact(df):
     df.sort_values(['MedicaidNum','OutcomeDateTime'],ascending=[True,False],inplace=True)
     df_pivot = df.pivot_table(index=['MedicaidNum','PatientID'],columns='ContactTypeSuccess',
                               values='OutcomeDateTime',aggfunc='first')
+    df_pivot['Successful Home Visit'] = df_pivot['Successful Home Visit'].dt.date
+    df_pivot['Successful Phone Contact'] = df_pivot['Successful Phone Contact'].dt.date
+    df_pivot['Unsuccessful Home Visit'] = df_pivot['Unsuccessful Home Visit'].dt.date
+    df_pivot['Unsuccessful Phone Contact'] = df_pivot['Unsuccessful Phone Contact'].dt.date
     return df_pivot
+
+def contactTypeCounter(df):
+    '''takes in contact dataframe and groups by patientID to count of contacts'''
+    contact_counts = df.groupby(['PatientID','MedicaidNum','ContactTypeSuccess'],as_index=False).count()
+    contact_counts = contact_counts.pivot_table(index=['MedicaidNum','PatientID'],columns='ContactTypeSuccess',values='ContactTS')
+    contact_counts.fillna(0,inplace=True)
+    col_names = {i:"Count of " + i for i in contact_counts.columns}
+    contact_counts.rename(columns=col_names,inplace=True)
+    contact_counts.reset_index(inplace=True)
+    return contact_counts
