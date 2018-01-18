7db6c13977fc11916e0df21419f6f319a3ae76e1
Garret Munoz <gmunoz@uic.edu>
2018-01-18 15:17:06 -0600

    adding new team wide connection object

---
+++ dbconnect/dbconnect.py
@@ -0,0 +1,54 @@
+import configparser
+from sqlalchemy import create_engine
+import pandas as pd
+from secret.secret import secret
+
+class DatabaseConnect():
+    def __init__(self,database):
+        self.hostname = secret().getHost()
+        self.username = secret().getUser()
+        self.password = secret().getSecret()
+        self.database = database
+
+    def connection_obj(self):
+        engine = create_engine("mysql+pymysql://{}:{}@{}:3306/{}".format(self.username,self.password,self.hostname,self.database))
+        connection = engine.connect()
+        return connection
+
+    def query(self,sql_str):
+        '''
+        sql_str(str): query to be fetched from data base
+        returns a pandas dataframe that contains query results
+        '''
+        try:
+            connection = self.connection_obj()
+            df = pd.read_sql(sql_str,con=connection)
+            connection.close()
+        except:
+            raise Exception
+        return df
+
+    def insert(self,df,tbl):
+        '''
+        df(pd.DataFrame): df to be inserted
+        tbl(str): table in database to insert df into
+        '''
+        try:
+            connection = self.connection_obj()
+            df.to_sql(name=tbl,con=connection,if_exists='append',index=False)
+            print('Successful append to {}.').format(tbl)
+            connection.close()
+        except:
+            raise Exception
+
+
+    def replace(self,df,tbl):
+        '''
+        df(pd.DataFrame): df to be inserted
+        tbl(str): table in database to insert df into
+        '''
+        try:
+            connection = self.connection_obj()
+            df.to_sql(name=tbl,con=connection,if_exists='replace',index=False)
+        except:
+            raise Exception
