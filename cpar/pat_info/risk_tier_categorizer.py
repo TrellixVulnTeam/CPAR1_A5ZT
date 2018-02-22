79b706c1a5e528247bb066e6f06577dbc7b694a4
Garret Munoz <gmunoz@uic.edu>
2018-02-22 13:53:54 -0600

    Adding UCSD risk score

--- cpar/pat_info/risk_tier_categorizer.py
+++ cpar/pat_info/risk_tier_categorizer.py
@@ -5,7 +5,7 @@ import numpy as np
 from datetime import timedelta, date
 from dbconnect import dbconnect
 
-release_num = 3020
+release_num = input("Enter in release num: ")
 connector = dbconnect.DatabaseConnect('CHECK_CPAR2')
 
 max_date = connector.query('''SELECT HFS_Release_Date FROM release_info where ReleaseNum = {}'''.format(release_num))
