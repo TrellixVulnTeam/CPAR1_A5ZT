4d1bcab159855655761663bfaaf685ccc5a61b48
Garret Munoz <gmunoz@uic.edu>
2018-03-14 15:54:00 -0500

    Adding data load initiator

---
+++ cpar/data_load/data_load_initiator.py
@@ -0,0 +1,30 @@
+import fire
+import sys
+from CHECK.dbconnect import dbconnect
+from CHECK.cpar.data_load.extract_and_rename_files import ExtractFiles
+from CHECK.cpar.data_load.hfs_load_data import HFSLoadData
+
+class DataLoadInitiator(object):
+
+    def __init__(self,release_num, release_date, database = 'CHECK_CPAR2', data_source = 'HFS'):
+
+        self.release_date = release_date
+        self.database = database
+        self.data_source = data_source
+        self.conn = dbconnect.DatabaseConnect(self.database)
+        self.release_num = release_num
+
+
+    def initiate_process(self):
+        print('Initiated')
+        self.conn.query("""Call pat_info_demo_load({})""".format(self.release_num), df_flag=False)
+        if ExtractFiles().create_and_unzip_files():
+            HFSLoad = HFSLoadData(self.database, self.release_num, self.data_source)
+            HFSLoad.load_data()
+            HFSLoad.inline_loader()
+
+if __name__ == '__main__':
+
+    fire.Fire(DataLoadInitiator)
+    #print('hello, lets start!')
+    #DataLoadInitiator('234','34','CHECK_CPAR2', 'hfs').initiate_process()
--- cpar/data_load/extract_and_rename_files.py
+++ cpar/data_load/extract_and_rename_files.py
@@ -1,32 +1,60 @@
 import os
 import zipfile
-import datetime
-
-current_dir = os.getcwd()
-zip_files = [i for i in os.listdir(current_dir) if i.endswith('CCCDMonthlyUICheck.zip')]
-assert len(zip_files) == 1
+from CHECK.secret import secret
 
 
-file_name = zip_files[0]
-source_file_path = current_dir+"/source_data/"
-output_file_path = current_dir+"/output_data/"
-path_to_zip_file = current_dir+'/'+file_name
-sql_file_path = current_dir+"/sql_scripts/"
-try:
-    if not os.path.exists(source_file_path):
-        os.makedirs(source_file_path)
-    if not os.path.exists(output_file_path):
-        os.makedirs(output_file_path)
-    if not os.path.exists(sql_file_path):
-        os.makedirs(sql_file_path)
-    if os.path.exists(path_to_zip_file):
-        os.rename(path_to_zip_file, source_file_path+file_name)
-except:
-    print("Something went wrong")
+class ExtractFiles(object):
 
-#unzip the file from source folder to output folder
+    def __init__(self):
 
-file_unzipped = zipfile.ZipFile(source_file_path+file_name, 'r')
-file_unzipped.setpassword(b'SprinG1023!')
-file_unzipped.extractall(output_file_path)
-file_unzipped.close()
+        self.current_dir = os.getcwd()
+        self.source_file_path = self.current_dir + "/source_data/"
+        self.output_file_path = self.current_dir + "/output_data/"
+        self.sql_file_path = self.current_dir + "/sql_scripts/"
+        self._sec = secret.secret()
+
+    def create_and_unzip_files(self):
+
+        if os.path.exists(self.output_file_path) and len([i for i in os.listdir(self.current_dir)]) == 13:
+            print('Files already present in output_data folder')
+            return True
+
+        if os.path.exists(self.source_file_path):
+            source_files = [i for i in os.listdir(self.source_file_path)]
+            if len(source_files) == 1:
+                print('Zip file present in source_data folder')
+                self.file_name = source_files[0]
+                if not os.path.exists(self.output_file_path):
+                    os.makedirs(self.output_file_path)
+                unzip_files()
+                return True
+
+        zip_files = [i for i in os.listdir(self.current_dir) if i.endswith('CCCDMonthlyUICheck.zip')]
+
+        if len(zip_files) != 1 :
+            print("Zip files not present in the current directory ", self.current_dir)
+            return False
+        else:
+            self.file_name = zip_files[0]
+            self.path_to_zip_file = self.current_dir + '/' + self.file_name
+            try:
+                if not os.path.exists(self.source_file_path):
+                    os.makedirs(self.source_file_path)
+                if not os.path.exists(self.output_file_path):
+                    os.makedirs(self.output_file_path)
+                if not os.path.exists(self.sql_file_path):
+                    os.makedirs(self.sql_file_path)
+                if os.path.exists(self.path_to_zip_file):
+                    os.rename(self.path_to_zip_file, self.source_file_path+self.file_name)
+                unzip_files()
+            except:
+                    print("Something went wrong")
+        return True
+
+    def unzip_files(self):
+        print('extracting files...')
+        # unzip the file from source folder to output folder
+        file_unzipped = zipfile.ZipFile(self.source_file_path + self.file_name, 'r')
+        file_unzipped.setpassword(self._sec.getZip())
+        file_unzipped.extractall(self.output_file_path)
+        file_unzipped.close()
